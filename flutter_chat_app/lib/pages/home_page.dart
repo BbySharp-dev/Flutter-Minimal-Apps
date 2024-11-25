a3103c811447a1557ec6ec4850cdcb3207a00adb
BbySharp-dev <doanminhtruong53.dev@gmail.com>
2024-11-25 22:46:02 +0700

    Menu Drawer

--- flutter_chat_app/lib/pages/home_page.dart
+++ flutter_chat_app/lib/pages/home_page.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/auth/auth_service.dart';
+import 'package:flutter_chat_app/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  // Method to handle logout logic
-  void logout() {
-    // get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        actions: [
-          // Logout button
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
-          ),
-        ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
